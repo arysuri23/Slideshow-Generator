c9cec2811f24343cd9e536adcdb2d68582db884f
arysuri <arysuri23@gmail.com>
2025-01-12 13:07:14 -0500

    update

--- slideGen.py
+++ slideGen.py
@@ -1,35 +1,44 @@
 from __future__ import print_function
 
-from apiclient import discovery
+from googleapiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
+import json
+import requests
 
 SCOPES = (
     'https://www.googleapis.com/auth/spreadsheets.readonly',
     'https://www.googleapis.com/auth/presentations',
     'https://www.googleapis.com/auth/drive'
 )
+
 store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
+    print('invalid')
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
 HTTP = creds.authorize(Http())
+print("authorized")
 SHEETS = discovery.build('sheets', 'v4', http=HTTP)
 SLIDES = discovery.build('slides', 'v1', http=HTTP)
+service = discovery.build('drive', 'v3', http=HTTP)
 
 print('** Fetch Sheets data')
-sheetID = '1zrh5YL5zmUa5QSVZkah24ShrQoy4e0pu1KXCc-Z5ARM'   # use your own!
-orders = SHEETS.spreadsheets().values().get(range='Form Responses 1',
+sheetID = '1ze6RSxdDzTyK3Zo7wN9NPm6rDCnjft76d3SzUaJq3vw'   # UPDATE WITH NEW SPREADSHEET
+orders = SHEETS.spreadsheets().values().get(range='Open House',
         spreadsheetId=sheetID).execute().get('values')
+print(orders)
 
 print('** Fetch chart info from Sheets')
 sheet = SHEETS.spreadsheets().get(spreadsheetId=sheetID,
-        ranges=['Form Responses 1']).execute().get('sheets')[0]
+        ranges=['Open House']).execute().get('sheets')[0]
 
 print('** Create new slide deck')
-DATA = {'title': 'Rush Powerpoint'}
-rsp = SLIDES.presentations().create(body=DATA).execute()
+
+slideshowid = "1QOr6e6Ple7z5ND9eGFy1_OxLcKWaImpgYRvQ-h1PDDs"
+rsp = SLIDES.presentations().get(presentationId=slideshowid).execute()
+
 deckID = rsp['presentationId']
 titleSlide = rsp['slides'][0]
 titleID = titleSlide['pageElements'][0]['objectId']
@@ -40,7 +49,7 @@ print('** Create slides & insert slide deck title+subtitle')
 
 
 reqs = [
-  {'insertText': {'objectId': titleID,    'text': 'Rush 2022'}},
+  {'insertText': {'objectId': titleID,    'text': 'Rush 2024'}},
  #{'insertText': {'objectId': subtitleID, 'text': ''}},
 ]
 rsp = SLIDES.presentations().batchUpdate(body={'requests': reqs},
@@ -49,15 +58,17 @@ rsp = SLIDES.presentations().batchUpdate(body={'requests': reqs},
 i = 1
 
 
-
 while i < len(orders):
     rusheeInfo = orders[i]
     rusheeName = rusheeInfo[1] + " " + rusheeInfo[2] + " #" + str(i)
-    bio = "Year: " + rusheeInfo[6] + "\nHometown: " + rusheeInfo[7] + "\nHigh School: " + rusheeInfo[8] + "\nUgliest Brother: " + rusheeInfo[10] + "\nHeads or Tails: " + rusheeInfo[11]
-    image = rusheeInfo[4]
+    print(i)
+    print(rusheeName)
+    bio = "Year: " + rusheeInfo[7] + "\nHometown: " +  rusheeInfo[8] + "\nHigh School: " + rusheeInfo[9] + "\nHidden Talent: " + rusheeInfo[11]
+    image = rusheeInfo[3]
     image_id = image.index("id=") + 3
     image_url = 'https://drive.google.com/uc?export=view&id=' + image[image_id:]  #https://drive.google.com/uc?export=view&id=' + image[image_id:]
-                                                            
+
+                 
     reqs = [
             {'createSlide': {'slideLayoutReference' : {'predefinedLayout': 'TITLE_AND_TWO_COLUMNS'}}}
         ]
@@ -99,5 +110,4 @@ while i < len(orders):
     rsp = SLIDES.presentations().batchUpdate(body = {'requests' : reqs},
             presentationId=deckID).execute().get('replies')
     i += 1
-#tableSlideID = rsp[0]['createSlide']['objectId']
-#chartSlideID = rsp[1]['createSlide']['objectId']
+
